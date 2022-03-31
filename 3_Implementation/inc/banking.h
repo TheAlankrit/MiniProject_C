3854b41739849103801c30674a65d146e89abeb4
Alankrit <88787598+TheAlankrit@users.noreply.github.com>
2022-03-31 13:31:37 +0530

    Create banking.h

---
+++ 3_Implementation/inc/banking.h
@@ -0,0 +1,14 @@
+#ifndef _cbank_
+void admin();
+void main_screen();
+void bank_menu();
+void create_new_account();
+void edit_details();
+void compute_transaction();
+float compute_interest(float t, float amount, int rate);
+void compute_delay(int j);
+void view_list();
+void erase_details();
+void see_details();
+void close_application();
+#endif
